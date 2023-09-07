d056f7636be6c3419815c80e1a21206a7eeddae5
be-great <begreat5333@gmail.com>
2023-09-07 14:35:58 -0700

    Task 8: write , low-level I/O function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Description: Display A string using low-level I/O function
+ * Return: Always 1 (Failure)
+ */
+int main(void)
+{
+
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(STDOUT_FILENO, str, sizeof(str) - 1);
+	return (0);
+}
