49f7169348d4079f7fcdf6b454145ce9611bd570
be-great <begreat5333@gmail.com>
2023-09-07 12:27:33 -0700

    Task 5 : printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ * Description: Display a String using printf function
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
