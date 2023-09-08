4812d0f72a53bc88fc0a84872744e469a29e281d
be-great <begreat5333@gmail.com>
2023-09-08 13:09:22 -0700

    update Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,22 +8,17 @@ int main(void)
 {
 	int digit0 = 0;
 	int digit1 = 0;
-	char ch0, ch1, ch2, ch3;
 
 	while (digit0 <= 98)
 	{
 		digit1 = digit0 + 1;
 		while (digit1 <= 99)
 		{
-			ch0 = '0' + (digit0 / 10);
-			ch1 = '0' + (digit0 % 10);
-			ch2 = '0' + (digit1 / 10);
-			ch3 = '0' + (digit1 % 10);
-			putchar(ch0);
-			putchar(ch1);
+			putchar('0' + (digit0 / 10));
+			putchar('0' + (digit0 % 10));
 			putchar(' ');
-			putchar(ch2);
-			putchar(ch3);
+			putchar('0' + (digit1 / 10));
+			putchar('0' + (digit1 % 10));
 			if ((digit0 + digit1) != 197)
 			{
 				putchar(',');
