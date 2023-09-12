155d8dc639289d971679733f6b50946f80418733
be-great <begreat5333@gmail.com>
2023-09-12 21:10:30 +0200

    update Task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,15 +10,17 @@ int main(void)
 	unsigned long n0 = 1;
 	unsigned long n1 = 2;
 	int i = 3;
+
 	printf("%lu, ", n0);
 	printf("%lu, ", n1);
-	while (i < 50)
+	while (i <= 50)
 	{
-		unsigned long n3 =n1+n0;
-		n0=n1;
-		n1=n3;
+		unsigned long n3 = n1 + n0;
+
+		n0 = n1;
+		n1 = n3;
 		printf("%lu", n3);
-		if (i < 49)
+		if (i < 50)
 			printf(", ");
 		i++;
 	}
