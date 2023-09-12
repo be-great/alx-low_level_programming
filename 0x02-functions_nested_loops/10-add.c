02aceca386a32da07c4c984b75699e8d7a61ae41
be-great <begreat5333@gmail.com>
2023-09-12 17:26:12 +0200

    Update Task 10 : a function that adds two integers and returns the result

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,10 +4,10 @@
  * add - function that adds two integers and returns the result.
  * @num1: first number
  * @num2: second number
- * Return: The sum of the numbers 
+ * Return: The sum of the numbers
  */
 
 int add(int num1, int num2)
 {
-	return num1+num2;
+	return (num1 + num2);
 }
