7d8fc7443a5531af56677d8b957639ea7c1d6f86
be-great <begreat5333@gmail.com>
2023-09-12 14:36:11 +0200

    Task 3 :check if char is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "_putchar.h"
+/**
+ * _islower - function that checks for lowercase character
+ * @c: character to test
+ * Return: 1 if char in lowercase , 0 if not
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int c);
 #endif
