891aa0c9b2be939cdaafbc6e6c0d8cde2f523e54
be-great <begreat5333@gmail.com>
2023-09-13 19:18:36 +0200

    Update task 3

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,15 +13,15 @@ int largest_number(int a, int b, int c)
 {
     int largest;
 
-    if (a > b && a > c)
+    if (a >= b && a >= c)
     {
         largest = a;
     }
-    else if (b > a && b > c)
+    else if (b >= a && b >= c)
     {
         largest = b;
     }
-    else if (c > b && c > a)
+    else if (c >= b && c >= a)
     {
         largest = c;
     }
--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -11,9 +11,9 @@ int main(void)
         int a, b, c;
         int largest;
 
-        a = 972;
+        a = -98;
         b = -98;
-        c = 0;
+        c = -98;
 
         largest = largest_number(a, b, c);
 
