c26ab8c3ff32fed52bc768324893e5f7551fc581
be-great <begreat5333@gmail.com>
2023-09-14 14:58:02 +0200

    Update Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,13 +7,14 @@
 
 void print_number(int n)
 {
+	int num = n;
 	if (n < 0)
 	{
-	n = n * -1;
+	num = num * -1;
 
 	_putchar('-');
 	}
-	if (n / 10 > 0)
-		print_number(n / 10);
-	_putchar(n % 10 + '0');
+	if (num / 10 > 0)
+		print_number(num / 10);
+	_putchar(num % 10 + '0');
 }
