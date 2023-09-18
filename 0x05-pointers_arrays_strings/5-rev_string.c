9f34e384f555631d020fbf6a88b4f7834ce92e01
be-great <begreat5333@gmail.com>
2023-09-18 20:42:06 +0200

    Task 5 : function reverse the string it self

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,14 +1,33 @@
 #include "main.h"
 
 /**
- * _islower - function that checks for lowercase character
- * @c: character to test
- * Return: 1 if char in lowercase , 0 if not
+ * rev_string - function that reverse a string
+ * @s: string to reverse
  */
 
-void rev_string(char *s);
+void rev_string(char *s)
 {
-	if (c >= 97 && c <= 122)
-		return (1);
-	return (0);
+	/*Declarations*/
+	int len;
+	int start;
+	int end;
+
+	if (!s)
+	{
+		return;
+	}
+	/* initlization*/
+	len = strlen(s);
+	start = 0;
+	end = len - 1;
+	/*operations*/
+	while (start < end)
+	{
+		char temp = s[start];
+
+		s[start] = s[end];
+		s[end] = temp;
+		start++;
+		end--;
+	}
 }
