ef1281458cc38a36ee41a00806c12d638bc7ba11
be-great <begreat5333@gmail.com>
2023-09-19 01:43:08 +0200

    Update Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,18 +11,19 @@ int _atoi(char *s)
 
 	int sign = 1;
 	unsigned int num = 0;
+	int i = 0;
 
-	while (*s)
+	while (s[i] != '\0')
 	{
-		if (*s == '-')
+		if (s[i] == '-')
 		sign = -1;
-		else if (*s >= '0' && *s <= '9')
+		else if (s[i] >= '0' && s[i] <= '9')
 		{
-			num = num * 10 + *s - '0';
+			num = num * 10 + s[i] - '0';
 		}
 		else if (num > 0)
 			break;
-		*s++;
+		i++;
 	}
 	return (num * sign);
 }
