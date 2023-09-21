ee61fdc670444f04baf9754cbde867af05383ddb
be-great <begreat5333@gmail.com>
2023-09-21 15:48:39 +0200

    Task 3 strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,20 +4,20 @@
  * _strcmp - function that compares two strings.
  *@s1: first option
  *@s2: second option
- *Return: negative number , postive number and zer0 
+ *Return: negative number , postive number and zer0
  */
 int _strcmp(char *s1, char *s2)
 {
 
-int i = 0;
-while (s1[i] != '\0' && s2[i] != '\0')
-{
-    if (s1[i] != s2[i])
-        return s1[i] - s2[i];
+	int i = 0;
 
-    i++;
-}
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
 
-return 0;
+	return (0);
 
 }
