d34dd2004097b3ab7838176ce33dfc298f850360
be-great <begreat5333@gmail.com>
2023-09-21 15:40:09 +0200

    Testing for loop

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,20 +1,22 @@
 #include "main.h"
 /**
- * _strcat - concatentate two strings
- * @dest: type char str 
- * @src: type char str 
- * Return: pointer to result dest
+ * _strncat - function that concatenates two strings.
+ * @dest: string one
+ * @src: string two
+ * @n: number of byte to copy
+ * Return: pointer of dest
  */
 
-char *_strncat(char *dest, char *src,int n)
+char *_strncat(char *dest, char *src, int n)
 {
-    int i;
-    for (i = 0; dest[i] != '\0' ; i++)
-    ;
-    for (int j = 0 ; src[j] != '\0' && n > 0; n--, i++,j++)
-    {
-        dest[i] = src[j];
-    }
-    dest[i] ='\0';
-    return (dest);
+	int i;
+
+	for (i = 0; dest[i] != '\0' ; i++)
+	;
+	for (int j = 0 ; src[j] != '\0' && n > 0; n--, i++, j++)
+	{
+	dest[i] = src[j];
+	}
+	dest[i] = '\0';
+	return (dest);
 }
