a82d168248ed529d976685790239e0d23687874b
be-great <begreat5333@gmail.com>
2023-10-04 22:25:31 +0200

    Task 0-4

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,19 +1,44 @@
 #include "main.h"
 /**
- * @brief Create a array object
- * 
- * 
- * @param size 
- * @param c 
- * @return char* 
+ * *str_concat - function that concatenates two strings.
+ * @s1: string one
+ * @s2: string two
+ * Return: pointer to the concat string
  */
 
 
 
 char *str_concat(char *s1, char *s2)
 {
+	int len1 = 0, len2 = 0;
+	char *s3;
+	int i, j;
+
+	if (s1 == NULL)
+		return ("");
+
+	if (s2 == NULL)
+		return ("");
+
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
 
 
+	s3 = (char *)malloc((len1 + len2 + 1) * sizeof(char));
 
+	if (s3 == NULL)
+	{
+		free(s3);
+		return (NULL);
+	}
+
+	for (i = 0; i < len1 ; i++)
+		s3[i] = s1[i];
+	for (i = len1, j = 0; j <= len2; j++, i++)
+		s3[i] = s2[j];
+
+	return (s3);
 
 }
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,19 +1,46 @@
 #include "main.h"
 /**
- * @brief Create a array object
- * 
- * 
- * @param size 
- * @param c 
- * @return char* 
- */
+* **alloc_grid - function that returns
+* a pointer to a 2 dimensional array of integers.
+* @width: column
+* @height: row
+* Return: the array
+*/
 
 
 
 int **alloc_grid(int width, int height)
 {
+	int **arr;
+	int i, j;
 
+	if (width <= 0 || height <= 0)
+		return (NULL);
 
+	arr = malloc((height) * sizeof(int));
 
+	if (arr == NULL)
+	{
+		free(arr);
+		return (NULL);
+	}
 
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = malloc(width * sizeof(int));
+
+		if (arr[i] == NULL)
+		{
+			for (i--; i >= 0 ; i--)
+				free(arr[i]);
+			free(arr);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			arr[i][j] = 0;
+	}
+	return (arr);
 }
