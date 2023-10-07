57aae786f971d998339910242a67d8238ca2a7fe
be-great <begreat5333@gmail.com>
2023-10-07 23:39:48 +0200

    Task 3 : function return array of values from the funciton argument from first argument to the othe argument min-max

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -8,6 +8,24 @@
 
 int *array_range(int min, int max)
 {
-	
+	int *ptr;
+	int i;
+	int j = 0;
+
+	if (min > max)
+		return (NULL);
+	ptr = malloc(((max - min) + 1) * sizeof(int *));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	i = min;
+	while (i <= max)
+	{
+		ptr[j] = i;
+		i++;
+		j++;
+	}
+	return (ptr);
 }
 
