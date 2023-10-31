8257385f275295234650b54df1502adc1f1129a3
be-great <begreat5333@gmail.com>
2023-10-31 02:02:16 +0200

    update 0-create-array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,7 +18,7 @@ char *create_array(unsigned int size, char c)
 	if (size <= 0)
 		return (NULL);
 
-	arr = (char *)malloc(size * sizeof(char));
+	arr = malloc(size * sizeof(char));
 	if (arr == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
