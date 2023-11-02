f77c3b22736947eacf0c0b1ef5444e25cf5397d5
be-great <begreat5333@gmail.com>
2023-11-03 00:12:10 +0200

    update Task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,164 +1,121 @@
-#include <stdio.h>
-#include <string.h>
+#include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
 
 /**
- * _isdigit - check if a string contine not digit char or not
- * @c: the string
- * Return: 1 if all is digit else 0
+ * _is_there_zero - determines if any number is zero
+ * @argv: the arguments values.
  */
-int _isdigit(char *c)
+void _is_there_zero(char *argv[])
 {
-	int i;
+	int i, isn1 = 1, isn2 = 1;
 
-	for (i = 0; c[i] != '\0' ; i++)
-	{
-		if (!(c[i] >= 48 && c[i] <= 57))
+	for (i = 0; argv[1][i]; i++)
+		if (argv[1][i] != '0')
 		{
-			return (0);
+			isn1 = 0;
+			break;
 		}
+
+	for (i = 0; argv[2][i]; i++)
+		if (argv[2][i] != '0')
+		{
+			isn2 = 0;
+			break;
+		}
+
+	if (isn1 == 1 || isn2 == 1)
+	{
+		printf("0\n");
+		exit(0);
 	}
-	return (1);
 }
 
 /**
- * *initzero - init array with zeros numbers
- * @arr: the array
- * Return: the array after init with zeros
+ * _initialize_array - init array with zeros
+ * @arr: The array.
+ * @length: length.
+ * Return: pointer to the init array.
  */
-char *initzero(char *arr, int len)
+char *_initialize_array(char *arr, int length)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i < len ; i++)
-	{
-		arr[i] = 0;
-	}
+	for (i = 0; i < length; i++)
+		arr[i] = '0';
+	arr[length] = '\0';
 	return (arr);
 }
 
 /**
- * multiply - the function that multiply the number
- * @num1: the first number
- * @num2: the second number
- * @result: num1 * num2
+ * _checknum - check the length of the number and
+ * that is a digit not a not number value
+ * @argv: arguments list value
+ * @n: rows values.
+ * Return: length of the number.
  */
-void multiply(char *num1, char *num2, char *result)
+int _checknum(char *argv[], int n)
 {
-	int len1 = strlen(num1);
-	int len2 = strlen(num2);
-	int i, j, sum, digit1, digit2, mul;
-	int resultLen, startIdx;
+	int len;
 
-	/* Initialize the result array with zeros*/
-	int *product = (int *)malloc((len1 + len2) * sizeof(int));
-
-	memset(product, 0, (len1 + len2) * sizeof(int));
-
-	/*Check for memory allocation failure*/
-	if (product == NULL)
-	{
-		printf("Memory allocation failed\n");
-		exit(1);
-	}
-
-	/* Multiply each digit and update the result array */
-	for (i = len1 - 1; i >= 0; i--)
-	{
-		for (j = len2 - 1; j >= 0; j--)
+	for (len = 0; argv[n][len]; len++)
+		if (!isdigit(argv[n][len]))
 		{
-			digit1 = num1[i] - '0';
-			digit2 = num2[j] - '0';
-			mul = digit1 * digit2;
-			sum = mul + product[i + j + 1];
-			product[i + j + 1] = sum % 10;
-			product[i + j] += sum / 10;
+			printf("Error\n");
+			exit(98);
 		}
-	}
 
-	/* Convert the product to a string*/
-	resultLen = len1 + len2;
-	startIdx = 0;
-	while (startIdx < resultLen && product[startIdx] == 0)
-	{
-		startIdx++;
-	}
-
-	if (startIdx == resultLen)
-	{
-		strcpy(result, "0");
-	}
-	else
-	{
-		for (i = startIdx; i < resultLen; i++)
-		{
-			result[i - startIdx] = product[i] + '0';
-		}
-			result[resultLen - startIdx] = '\0';
-	}
+	return (len);
 }
+
 /**
- * main - multiply two number that are large in number
- * @argc: the number of argument
- * @argv: the argument it self
+ * main - The Entry point.
+ * discription: a program that multiplies two positive numbers.
+ * @argc: number of arguments.
+ * @argv: arguments list.
  * Return: 0 always (Success)
  */
 int main(int argc, char *argv[])
 {
-	char *arr;
-	int len1, len2;
-	int i, j, k, ca, addl,add, lnout;
-	/*argument checks*/
+	int len1, len2, lnout, add, addl, i, j, k, ca;
+	char *nout;
+
 	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	if (!_isdigit(argv[1]) || !_isdigit(argv[2]))
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	len1 = strlen(argv[1]);
-	len2 = strlen(argv[2]);
-	arr = (char *)malloc(len1 + len2 + 1);
-	if (arr == NULL)
-	{
-		printf("Error\n");
-		return (0);
-	}
-	arr = initzero(arr, (len1 + len2));
-	/* multiply method start by the end*/
-	lnout = len1 + len2;
-	k = lnout - 1, i = len1 - 1, j = len2 - 1, ca = addl = 0;
+		printf("Error\n"), exit(98);
+	len1 = _checknum(argv, 1), len2 = _checknum(argv, 2);
+	_is_there_zero(argv), lnout = len1 + len2, nout = malloc(lnout + 1);
+	if (nout == NULL)
+		printf("Error\n"), exit(98);
+	nout = _initialize_array(nout, lnout), k = lnout - 1;
+	i = len1 - 1, j = len2 - 1, ca = addl = 0;
 	for (; k >= 0; k--, i--)
 	{
-		if (i < 0)
+		if (i < 0)/*The number two is larger than the other one*/
 		{
 			if (addl > 0)
 			{
-				add = (arr[k] - '0') + addl;
+				add = (nout[k] - '0') + addl;
 				if (add > 9)
-					arr[k - 1] = (add / 10) + '0';
-				arr[k] = (add % 10) + '0';
+					nout[k - 1] = (add / 10) + '0';
+				nout[k] = (add % 10) + '0';
 			}
 			i = len1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
 		}
-		if (j < 0)
+		if (j < 0)/*The number one is larger than the number two*/
 		{
-			if (arr[0] != '0')
+			if (nout[0] != '0')
 				break;
 			lnout--;
-			free(arr), arr = malloc(lnout + 1), arr = initzero(arr, lnout);
+			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
 			k = lnout - 1, i = len1 - 1, j = len2 - 1, ca = addl = 0;
 		}
 		if (j >= 0)
 		{
-			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (arr[k] - '0') + addl;
-			addl = add / 10, arr[k] = (add % 10) + '0';
+			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
+			addl = add / 10, nout[k] = (add % 10) + '0';
 		}
 	}
-
-	printf("%s\n", arr);
+	printf("%s\n", nout);
 	return (0);
 }
