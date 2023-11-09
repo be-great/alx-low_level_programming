c55d7dac76e466b456139e0f940ed651a617c080
be-great <begreat5333@gmail.com>
2023-11-09 02:48:52 +0200

    Task 2 : int_index pcommit was for Task 1 array_iterator

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,13 +1,11 @@
 #include "function_pointers.h"
 
 /**
- * int_index - searches for an integer
- * @array: input integer array.
+ * int_index - function that searches for an integer.
+ * @array: The array of int.
  * @size: size of the array.
- * @cmp: pointer to the function to be used
- * to compare values.
- *
- * Return: index of the first eement for which the cmp
+ * @cmp: is a pointer to the function to be used to compare values
+ * Return: The index of the first element for which the cmp
  * function does not return 0. If no elements matches,
  * return -1. If size <= 0, return -1.
  */
@@ -17,9 +15,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (array && cmp)
 	{
+		/*First check size*/
 		if (size <= 0)
 			return (-1);
-
+		/* loop throw the array and compare*/
 		for (i = 0; i < size; i++)
 			if (cmp(array[i]))
 				return (i);
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -6,5 +6,9 @@
 |---|---|
 |0-print_name|function that prints a name.|
 |1-array_iterator|function that executes a function given as a parameter on each element of an array.|
-|||
-|||
+|2-int_index||
+|3-main.c|contain our main function to task 3|
+|3-op_functions|This file  contain the 5 following functions : `op_add`,`op_sub`,`op_mul`,`op_div`,`op_mod` |
+|3-get_op_func.c| This file contain the function that selects the correct function to perform the operation asked by the user.|
+|3-calc.h|This file contain all the function prototypes and data structures used by the program 3-main.c|
+|100-main_opcodes|program that prints the opcodes of its own main function.|
