556084142c2f0ae7dc74cfb7935223ebfa1b73d7
be-great <begreat5333@gmail.com>
2023-11-21 12:52:35 +0200

    Update Task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -17,7 +17,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		while (size != (index - 1) && prev != NULL)
 		{
 			prev = prev->next;
-
+			size++;
 		}
 	}
 	if (prev == NULL || (prev->next == NULL && index != 0))
