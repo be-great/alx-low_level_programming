5c503b5e9059b18c84d24ac3684e230e4e7b3def
Ahed Eisa <78013422+be-great@users.noreply.github.com>
2023-11-21 17:52:24 +0200

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,1 +1,20 @@
 #include "lists.h"
+
+/**
+* print_listint_safe - function that prints a listint_t linked list
+* @head: the node head
+* Return: the number of node in the list
+* Description: 1 ->  2 -> 3
+*                    |___ |
+* This is a cycle we need to detect where that happend return the header that do that (3)
+*/
+
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t size = 0;
+
+
+
+	return (size);
+}
