cba6667c2fe1c318cfc80e1a57e1a1f7a3e7b738
be-great <begreat5333@gmail.com>
2023-11-21 12:44:43 +0200

    update Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -43,6 +43,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		newnode->next = tempnode->next;
 		tempnode->next = newnode;
 	}
-	return (new);
+	return (newnode);
 
 }
