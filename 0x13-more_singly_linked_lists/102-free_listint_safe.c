80d3bee3425dae18edc35bc8ff8aaade52e49c19
be-great <begreat5333@gmail.com>
2023-11-21 23:27:48 +0200

    add task 13 and 14

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,1 +1,76 @@
 #include "lists.h"
+
+/**
+* free_listp2_ - a function that frees a listp list.
+* @head: the linkend list header
+*/
+void free_listp2_(listp **head)
+{
+	listp *tempnode, *temp2;
+
+	if (head != NULL)
+	{
+		temp2 = *head;
+		tempnode = temp2;
+		while (tempnode != NULL)
+		{
+
+			temp2 = (temp2)->next;
+			free(tempnode);
+			tempnode = temp2;
+		}
+		*head = NULL;
+	}
+
+}
+
+
+/**
+* free_listint_safe - This function can free lists with a loop
+* @h: the node head
+* Return: the size of the list that was free
+* Description: The function sets the head to NULL
+*/
+size_t free_listint_safe(listint_t **h)
+{
+	size_t size = 0;
+	listp *new, *ptr = NULL;
+	listint_t *temp;
+
+	/* create the new storing array, check for freeing**/
+	while (*h)
+	{
+		new = malloc(sizeof(listp));
+		if (!new)
+		{
+			free(new);
+			exit(98);
+		}
+		new->p = (void *)*h;
+		new->next = ptr;
+		ptr = new;
+		/* loop to check we didn't get that headers*/
+		/* NULL <- p1 <- p2 <- p3*/
+		new = ptr;
+		while (new->next != NULL)
+		{
+			new = new->next;
+			if (*h == new->p)
+			{
+				/* free the new struct object you create*/
+				*h = NULL;
+				free_listp2_(&ptr);
+				return (size);
+			}
+
+		}
+	    temp = *h;
+		*h = (*h)->next;
+		free(temp);
+		size++;
+	}
+	*h = NULL;
+	/* free the new struct you created*/
+	free_listp2_(&ptr);
+	return (size);
+}
--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,1 +1,32 @@
 #include "lists.h"
+
+/**
+* *find_listint_loop -  function that finds the loop in a linked list.
+* @head: the node head
+* Return: The address of the node where the loop starts
+* or NULL if there is no loop
+* rules: 1 -You are not allowed to use malloc, free or arrays
+*        2- You can only declare a maximum of two variables in your function
+*/
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow = head, *fast = head;
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (slow);
+		}
+	}
+
+	return (NULL);
+}
