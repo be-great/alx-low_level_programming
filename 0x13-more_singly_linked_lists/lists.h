ad3884396d1a912adb8c09e9df8b2cbd5ce7ccdf
be-great <begreat5333@gmail.com>
2023-11-21 18:18:21 +0200

    what??git add .

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -16,6 +16,17 @@ typedef struct listint_s
 
 	struct listint_s *next;
 } listint_t;
+/**
+ * struct listp_s - singly linked list
+ * @p: pointer of the node
+ * @next: points to the next node
+ */
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+} listp_t;
+
 listint_t *add_nodeint(listint_t **head, const int n);
 
 listint_t *add_nodeint_end(listint_t **head, const int n);
@@ -33,4 +44,5 @@ int pop_listint(listint_t **head);
 size_t print_listint(const listint_t *h);
 int sum_listint(listint_t *head);
 listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
 #endif
