af1c5958a0c7fc72d453e5c165919fa91f8684a0
be-great <begreat5333@gmail.com>
2023-11-23 16:24:04 +0200

    task1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
- * @brief 
- * 
- * @param n 
+ * print_binary -  function that prints the binary representation of a number.
+ * @n: the number
  */
-
-
 void print_binary(unsigned long int n)
-{}
+{
+	if (n >> 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
