6ef7af6845eaa35be07c8d402385daf199b532d6
be-great <begreat5333@gmail.com>
2023-11-29 00:51:53 +0200

    update Task 4

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -129,20 +129,26 @@ void print_osabi(unsigned char *e_ident)
 
 	switch (e_ident[EI_OSABI])
 	{
-	case ELFOSABI_NONE:
-		printf("UNIX - System V\n");
-		break;
-	case ELFOSABI_HPUX:
-		printf("UNIX - HP-UX\n");
-		break;
 	case ELFOSABI_NETBSD:
 		printf("UNIX - NetBSD\n");
 		break;
 	case ELFOSABI_LINUX:
 		printf("UNIX - Linux\n");
 		break;
+	case ELFOSABI_NONE:
+		printf("UNIX - System V\n");
+		break;
+	case ELFOSABI_HPUX:
+		printf("UNIX - HP-UX\n");
+		break;
 	case ELFOSABI_SOLARIS:
 		printf("UNIX - Solaris\n");
+		break;
+	case ELFOSABI_ARM:
+		printf("ARM\n");
+		break;
+	case ELFOSABI_STANDALONE:
+		printf("Standalone App\n");
 		break;
 	case ELFOSABI_IRIX:
 		printf("UNIX - IRIX\n");
@@ -153,12 +159,7 @@ void print_osabi(unsigned char *e_ident)
 	case ELFOSABI_TRU64:
 		printf("UNIX - TRU64\n");
 		break;
-	case ELFOSABI_ARM:
-		printf("ARM\n");
-		break;
-	case ELFOSABI_STANDALONE:
-		printf("Standalone App\n");
-		break;
+
 	default:
 		printf("<unknown: %x>\n", e_ident[EI_OSABI]);
 	}
@@ -190,17 +191,17 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
 
 	switch (e_type)
 	{
-	case ET_NONE:
-		printf("NONE (None)\n");
-		break;
-	case ET_REL:
-		printf("REL (Relocatable file)\n");
-		break;
 	case ET_EXEC:
 		printf("EXEC (Executable file)\n");
 		break;
 	case ET_DYN:
 		printf("DYN (Shared object file)\n");
+		break;
+	case ET_NONE:
+		printf("NONE (None)\n");
+		break;
+	case ET_REL:
+		printf("REL (Relocatable file)\n");
 		break;
 	case ET_CORE:
 		printf("CORE (Core file)\n");
