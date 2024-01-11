0a4af6873ad84118c848b66f920272fbb1e33610
be-great <begreat5333@gmail.com>
2024-01-12 00:27:09 +0300

    Task

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,6 +1,18 @@
 #include "lists.h"
 
+/**
+* sum_dlistint - sum the list at giving point
+* @head: the header
+* Return: the sum of all data
+*/
 int sum_dlistint(dlistint_t *head)
 {
-    
+	size_t sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
 }
