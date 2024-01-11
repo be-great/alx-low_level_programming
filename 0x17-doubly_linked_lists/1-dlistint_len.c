f5c306865db6193d9ddf278ec483c33000ba490b
be-great <begreat5333@gmail.com>
2024-01-11 23:21:01 +0300

    Task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,6 +1,18 @@
 #include "lists.h"
-
+/**
+* dlistint_len - function that returns the number of elements in a linked
+* dlistint_t list.
+* @h: the header
+* Return: the length of the linkend list
+*/
 size_t dlistint_len(const dlistint_t *h)
 {
-    
+	size_t size = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		size++;
+	}
+	return (size);
 }
