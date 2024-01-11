589f8784797de1c6886be3c4fa552c7bffa51e66
be-great <begreat5333@gmail.com>
2024-01-12 00:25:15 +0300

    node at giving index 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,6 +1,23 @@
 #include "lists.h"
-
+/**
+* get_dnodeint_at_index - Get the dnodeint at index object
+* @head: the head of the list
+* @index: the index to get
+* Return: the node at giving index or null if not found
+*/
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    
+	unsigned int count = 0;
+
+	dlistint_t *current = head;
+
+	while (current != NULL)
+	{
+		if (count == index)
+			return (current);
+		count++;
+		current = current->next;
+	}
+
+	return (NULL);
 }
