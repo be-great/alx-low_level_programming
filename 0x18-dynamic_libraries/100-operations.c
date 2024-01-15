479981e496a14877466352723734bdaaa360498c
be-great <begreat5333@gmail.com>
2024-01-15 09:32:26 -0800

    task 3

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,59 @@
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+/**
+ * add - returns the sum of a and b.
+ * @a: number one.
+ * @b: number two.
+ * Return: a+b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - returns the difference of a and b
+ * @a: number one.
+ * @b: number two.
+ * Return: a-b.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - returns the product of a and b.
+ * @a: number one.
+ * @b: number two.
+ * Return: a * b.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - returns the result of the division of a by b.
+ * @a: number one.
+ * @b: number two.
+ * Return: a/b.
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod -  returns the remainder of the division of a by b.
+ * @a: number one.
+ * @b: number two.
+ * Return: a % b.
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
