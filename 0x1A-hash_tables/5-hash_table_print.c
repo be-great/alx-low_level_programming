8c4fee7893b284369584679cb027b18aef75cc23
be-great <begreat5333@gmail.com>
2024-02-02 16:47:00 +0300

    task

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -2,11 +2,34 @@
 
 
 /**
- * hash_table_print - function that prints a hash table.
- * @ht: the hash table
- */
+* hash_table_print - function that prints a hash table.
+* @ht: the hash table
+*/
 void hash_table_print(const hash_table_t *ht)
 {
+	hash_node_t *nodes;
 
+	unsigned long int i;
 
+	if (!ht)
+		return;
+
+	putchar('{');
+	for (i = 0; i < ht->size; i++)
+	{
+		nodes = ht->array[i];
+		while (nodes != NULL)
+		{
+			if (i == 0)
+				printf("'%s': '%s'", nodes->key, nodes->value);
+			else
+			{
+				printf(", ");
+				printf("'%s': '%s'", nodes->key, nodes->value);
+			}
+			nodes = nodes->next;
+		}
+	}
+	putchar('}');
+	putchar('\n');
 }
