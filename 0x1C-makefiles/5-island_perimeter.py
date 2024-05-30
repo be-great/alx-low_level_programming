403aa96e6b6bca9bed65a5333edffa947a29e46b
be-great <begreat5333@gmail.com>
2024-05-30 03:36:56 +0300

    update 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,4 +1,34 @@
 #!/usr/bin/python3
 """
-5-main
+island perimeter
+water if above == 0 or we are the begining of the array
+water if blow == 0 or we are the end of the array
+water if right == 0 or we are the begining of the column
+water if left == 0 or we are the end of the column 
 """
+
+def island_perimeter(grid):
+    """The island perimeter"""
+    count = 0
+    for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if (grid[i][j] == 1):
+                    # above
+                    if (i == 0 or grid[i - 1][j] == 0)
+                        count += 1
+                    # blow
+                    if (i == len(grid[i]) - 1 or grid[i + 1][j] == 0):
+                        count += 1
+                    # check left
+                    if (j == 0 or grid[i][j - 1] == 0):
+                        count += 1
+                    # check right
+                    if (j == len(grid[i]) - 1 or grid[i][j + 1]):
+                        count += 1
+        
+    return (count)
+
+
+
+
+
